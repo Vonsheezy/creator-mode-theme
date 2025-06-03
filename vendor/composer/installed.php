b338c712216205fd33bbbe22e938b5ee91694a79
Eliasu Abraman <sabalisfi@yahoo.com>
2025-06-03 19:02:19 +0000

    Allow vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'eliasu/holy-canvas',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3b7457ccb57679bf4dd1fbf319aa73f66e4ca663',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'eliasu/holy-canvas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3b7457ccb57679bf4dd1fbf319aa73f66e4ca663',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
